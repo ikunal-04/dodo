3cba65b85ad7dffde15a25ba2270088106ce2110
Kunal Garg <kunalgarg054@gmail.com>
2025-11-21 17:28:14 +0530

    project init, sidebar done

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import * as React from "react"
+import {
+  ArrowLeftRight,
+  CreditCard,
+  Command,
+  Frame,
+  LifeBuoy,
+  Map,
+  Headphones,
+  Send,
+  Settings2,
+  Grid2x2,
+  Clock,
+  NotepadText,
+  History,
+  Settings
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+const data = {
+  user: {
+    name: "Arthur Taylor",
+    email: "arthur@alignui.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: Grid2x2,
+      isActive: true,
+    },
+    {
+      title: "My Cards",
+      url: "#",
+      icon: CreditCard,
+    },
+    {
+      title: "Transfer",
+      url: "#",
+      icon: ArrowLeftRight,
+    },
+    {
+      title: "Transactions",
+      url: "#",
+      icon: History,
+    },
+    {
+      title: "Payments",
+      url: "#",
+      icon: NotepadText,
+    },
+    {
+      title: "Exchange",
+      url: "#",
+      icon: Clock,
+    },
+  ],
+  projects: [
+    {
+      name: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+    {
+      name: "Support",
+      url: "#",
+      icon: Headphones,
+    }
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild className="border-b py-8">
+              <a href="#">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={"/logo.svg"} alt={"logo"} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Apex</span>
+                  <span className="truncate text-xs">Finance & Banking</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
